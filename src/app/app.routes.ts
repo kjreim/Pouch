06797c326831bf4250865a70cea82b903d6e9037
onionknight <kjreimers461@gmail.com>
2025-01-12 12:36:23 -0600

    created initial application with material theme, side nav menu, and using pouchdb
    (cherry picked from commit a48ea883ad5b668dd4a79c77458dd566a6c1e0d0)

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import {StripComponent} from './components/strip/strip.component';
+import {HomeComponent} from './components/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'Strips',
+    component: StripComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'Home',
+    pathMatch: 'full',
+  }
+];
